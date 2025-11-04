3b04df3a7254e0b66efc98d33e0b0bd0c99290aa
Cristian Figueroa Ferrer <ilandbz@gmail.com>
2025-11-03 21:51:11 -0500

    asd

--- app/Models/Archivo.php
+++ app/Models/Archivo.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Archivo extends Model
 {
-    //
+    protected $fillable = [
+        'documento_id',
+        'nro',
+        'ruta_archivo',
+        'url', // opcional si luego agregas la URL pública
+    ];
+
+    // Relación con Documento
+    public function documento()
+    {
+        return $this->belongsTo(Documento::class);
+    }
 }
